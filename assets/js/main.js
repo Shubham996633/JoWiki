78d24455a56a07be1316dd7608811952410c50ab
Shubham Maurya <risjiomaujio7654@gmail.com>
2022-10-06 13:58:19 +0530

    update

---
+++ assets/js/main.js
@@ -0,0 +1,30 @@
+
+
+import {
+  setSearchFocus,
+  
+} from "../js/searchBar.js";
+
+document.addEventListener("readystatechange", (event) => {
+  if (event.target.readyState === "complete") {
+    initApp();
+  }
+});
+
+const initApp = () => {
+  setSearchFocus();
+  
+  const form = document.getElementById("searchBar");
+  form.addEventListener("submit", submitTheSearch);
+};
+
+// Procedural "workflow" function
+const submitTheSearch = (event) => {
+  event.preventDefault();
+  
+  processTheSearch();
+  setSearchFocus();
+};
+
+
+// 11:11:23
